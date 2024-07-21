c9f6ac79d2fffb6a27cd9506101c88ce75a234bd
hyejuuuwu <163059245+hyejuuuwu@users.noreply.github.com>
2024-07-21 02:33:50 -0400

    Add files via upload

---
+++ minions/2.Raiz i-esima/Raiz i-esimav2.py
@@ -0,0 +1,31 @@
+import math
+
+def main():
+    numero,i,raiz = 0.0, 0.0, 0.0
+    
+    while True:
+        try:
+            numero = float(input())
+            if numero < 0:
+                print("Error,debe ser >= 0")
+            else:
+                break
+        except ValueError:
+            print("ERROR, entrada no valida. Ingrese un numero")
+            
+    while True:
+        try:
+            i = float(input())
+            if i<= 0:
+                print("ERROR, debe ser > 0")
+            else:
+                break
+        except ValueError:
+            print("ERROR, entrada no valida, ingrese un numero")
+    
+    raiz= numero ** ( 1 / i )
+    
+    print("{:.3f}".format(raiz))
+    
+    if __name__ == "__main__":
+        main()
